5de664b4279b6652c0f6aff5d5c6ee82b8e292c7
Brian-Agustin <braiian.dlv@gmail.com>
2022-08-31 14:47:36 -0300

    creando mi primera plantilla

---
+++ AppCoder/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from AppCoder.views import curso, inicio
+
+urlpatterns = [
+    path('', inicio),
+    path('curso/', curso, name='AppCoder_Curso')
+]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from AppCoder.models import Curso
 # Create your views here.
+
+def inicio(request):
+    return render(request, 'index.html')
+
 def curso(request):
     curso1 = Curso(nombre='python', camada=31095)
     curso1.save()
     contexto = {
         'curso': curso1
     }
-    return render(request, 'curso.html', contexto)
+    return render(request, 'AppCoder/curso.html', contexto)
+
+
 
