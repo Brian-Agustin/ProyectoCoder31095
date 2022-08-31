c58337d8dcc60db19e29a3b97eff5119efda27c3
Brian-Agustin <braiian.dlv@gmail.com>
2022-08-31 19:33:44 -0300

    creando las plantillas con herencia desde la plase padre

--- AppCoder/admin.py
+++ AppCoder/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from AppCoder.models import Curso, Estudiantes
+
+admin.site.register(Curso)
+admin.site.register(Estudiantes)
--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from AppCoder.views import curso, inicio
 
 urlpatterns = [
-    path('', inicio),
+    path('', inicio, name='AppCoder_Inicio'),
     path('curso/', curso, name='AppCoder_Curso')
 ]
